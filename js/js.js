edba2a132198ba04bf0dbdb3063118819e3ee147
rxy686 <100482015+rxy686@users.noreply.github.com>
2022-11-21 13:30:18 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -497,7 +497,7 @@ option = {
                     name: '胃肠道疾病'
                 },
                 {
-                    value: 40,
+                    value: 40 ,
                     name: '呼吸道疾病'
                 },
                 {
